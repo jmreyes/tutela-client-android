9513226b734a1772d439720cf62afc5991203c86
Juanma Reyes <juanma.resp@gmail.com>
2014-11-09 00:45:22 +0100

    Implement navigation drawer with animated button for Doctors (still no layout)

--- tutela/src/main/java/net/jmreyes/tutela/ui/doctor/main/DoctorMainActivity.java
+++ tutela/src/main/java/net/jmreyes/tutela/ui/doctor/main/DoctorMainActivity.java
@@ -1,7 +1,10 @@
 package net.jmreyes.tutela.ui.doctor.main;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import net.jmreyes.tutela.R;
 import net.jmreyes.tutela.ui.common.BaseActivity;
 
@@ -25,7 +28,35 @@ public class DoctorMainActivity extends BaseActivity implements DoctorMainView {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
+
+            DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+
+            ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(
+                    this,  drawerLayout, toolbar,
+                    R.string.navigation_drawer_open, R.string.navigation_drawer_close
+            ){
+                public void onDrawerClosed(View view)
+                {
+                    super.onDrawerClosed(view);
+                    invalidateOptionsMenu();
+                    syncState();
+                }
+
+                public void onDrawerOpened(View drawerView)
+                {
+                    super.onDrawerOpened(drawerView);
+                    invalidateOptionsMenu();
+                    syncState();
+                }
+            };
+
+            drawerLayout.setDrawerListener(drawerToggle);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+
+            drawerToggle.syncState();
         }
+
     }
 
 
